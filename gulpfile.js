18ab217a98a6aacd477d8e5f7bd2bde132a9fb4c
bawn92 <gearoidmoc@gmail.com>
2015-09-13 19:49:16 +0100

    Started the contect me section, with woriking icons

--- gulpfile.js
+++ gulpfile.js
@@ -81,7 +81,9 @@ gulp.task('jade', function(){
  */
 gulp.task('watch', function () {
     gulp.watch('assets/css/**', ['sass']);
+    gulp.watch('assets/js/**', ['jekyll-rebuild']);
     gulp.watch(['index.html', '_layouts/*.html', '_includes/*'], ['jekyll-rebuild']);
+    gulp.watch('assets/js/**', ['jekyll-rebuild']);
     gulp.watch('_jadefiles/*.jade', ['jade']);
 });
 
